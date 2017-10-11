729d5f0b88e38dbab524130bc980cc455952e962
ajdavey8 <ajdavey8@gmail.com>
2017-10-11 17:01:03 +0100

    changed each method to while

--- directory.rb
+++ directory.rb
@@ -6,7 +6,7 @@ def input_students
   name = gets.chomp
 
   while !name.empty? do
-    students << {name: name, cohort: :november}
+    students << {name: name, cohort: "november"}
     puts "Now we have #{students.count} students"
     name=gets.chomp
   end
@@ -19,11 +19,10 @@ def print_header
 end
 
 def print(students)
-    students.each.with_index(1) do |student, index|
-      if student[:name].length > 12
-      else
-        puts "#{index}. #{student[:name]} (#{student[:cohort].capitalize} cohort)"
-    end
+  count = 0
+  while students.count > count
+    puts "#{students[count][:name]} (#{students[count][:cohort]})"
+    count = count + 1
   end
 end
 
