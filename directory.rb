c298cb542f7e58163c5da4e475c82b872ffb1b6d
ajdavey8 <ajdavey8@gmail.com>
2017-10-11 22:36:25 +0100

    user can add cohort aswell as name

--- directory.rb
+++ directory.rb
@@ -1,21 +1,22 @@
+@cohorts = [:january, :february, :march, :april, :may, :june,
+   :july, :august, :september, :october, :november, :december]
+@students = []
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-
-  students = []
   name = gets.chomp
-  puts "please enter student's age"
-  age = gets.chomp
-
   while !name.empty? do
-    students << {name: name, cohort: :november, age: age}
-    puts "Now we have #{students.count} students"
+  puts "And which cohort are they in?"
+  cohort=gets.chomp
+    while !@cohorts.include? cohort.to_sym
+      puts "Please enter valid a cohort"
+      cohort=gets.chomp
+    end
+    @students << {name: name, cohort: cohort.to_sym}
+    puts "Now we have #{@students.count} students. Please add another student or exit"
     name=gets.chomp
-    break if name.empty?
-    puts "And student's age?"
-    age=gets.chomp
   end
-  students
+  @students
 end
 
 def print_header
@@ -26,7 +27,7 @@ end
 def print(students)
   count = 0
   while students.count > count
-    puts "#{students[count][:name]}, #{students[count][:age]} (#{students[count][:cohort]})".center(50)
+    puts "#{students[count][:name]}, (#{students[count][:cohort]})".center(50)
     count = count + 1
   end
 end
