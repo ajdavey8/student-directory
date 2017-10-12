1341a3cb4b7b47547387ad597a08618bf9100599
ajdavey8 <ajdavey8@gmail.com>
2017-10-12 13:13:24 +0100

    added interactive menu

--- directory.rb
+++ directory.rb
@@ -1,6 +1,27 @@
 @cohorts = [:january, :february, :march, :april, :may, :june,
    :july, :august, :september, :october, :november, :december]
 @students = []
+def interactive_menu
+  loop do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    selection = gets.chomp
+
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit
+    else
+      puts "I don't know what you mean, please try again"
+    end
+  end
+end
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
