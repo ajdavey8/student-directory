7d111b6cc213302ac9552fdf4094aef2b14d8164
ajdavey8 <ajdavey8@gmail.com>
2017-10-13 12:36:39 +0100

    refac adding info to array

--- directory.rb
+++ directory.rb
@@ -4,7 +4,7 @@
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
@@ -42,17 +42,17 @@ end
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  name = gets.chomp
-  while !name.empty? do
+  @name = STDIN.gets.chomp
+  while !@name.empty? do
   puts "And which cohort are they in?"
-  cohort=gets.chomp
-    while !@cohorts.include? cohort.to_sym
+  @cohort = STDIN.gets.chomp
+    while !@cohorts.include? @cohort.to_sym
       puts "Please enter valid a cohort"
-      cohort=gets.chomp
+      @cohort = STDIN.gets.chomp
     end
-    @students << {name: name, cohort: cohort.to_sym}
+    add_student
     student_counter
-    name=gets.chomp
+    @name = STDIN.gets.chomp
   end
 end
 
@@ -93,13 +93,29 @@ def save_students
   file.close
 end
 
-def load_students
-  file = File.open("students.csv", "r")
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
-  name, cohort = line.chomp.split(',')
-    @students << {name: name, cohort: cohort.to_sym}
+  @name, @cohort = line.chomp.split(',')
+    add_student
   end
   file.close
 end
 
+def try_load_students
+  filename = ARGV.first
+  return if filename.nil?
+  if File.exists?(filename)
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else
+    puts "Sorry #{filename} doesn't exist"
+    exit
+  end
+end
+
+def add_student
+  @students << {name: @name, cohort: @cohort.to_sym}
+end
+try_load_students
 interactive_menu
